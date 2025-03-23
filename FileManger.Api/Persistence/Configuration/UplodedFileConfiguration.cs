f6ec4fd74c4e66b2416a0b0e432d86e354115e96
ITcharDO <dvsfv527@gmail.com>
2025-03-23 18:04:32 +0200

    Add project files.

---
+++ FileManger.Api/Persistence/Configuration/UplodedFileConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace FileManger.Api.Persistence.Configuration;
+
+public class UplodedFileConfiguration : IEntityTypeConfiguration<UplodedFile>
+{
+    public void Configure(EntityTypeBuilder<UplodedFile> builder)
+    {
+        builder
+            .Property(f => f.FileName)
+            .HasMaxLength(250);
+        builder
+            .Property(f => f.StoredFileName)
+            .HasMaxLength(250);
+        builder
+            .Property(f => f.ContentType)
+            .HasMaxLength(50);
+        builder
+            .Property(f => f.FileExtenstion)
+            .HasMaxLength(10);
+
+    }
+}
---
+++ FileManger.Api/Program.cs
@@ -0,0 +1,33 @@
+using FileManger.Api.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection string 'DefaultConnection' is not found");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
